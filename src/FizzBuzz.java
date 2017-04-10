5074f126039b5eeef9c792e745e4702b32091785
rylan <rylangooch@gmail.com>
2017-04-10 08:33:55 +0100

    Refactored FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,16 +4,33 @@
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        for (int x = 1; x < 100; x++) {
-            if (x % 3 == 0 && x % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if(x % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (x % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(x);
-            }
+        FizzBuzz FB = new FizzBuzz();
+        for (int x = 1; x <= 100; x++) {
+            FB.go(x);
         }
+    }
+
+    private void go(int n) {
+        if (DivisableByThreeAndFive(n)) {
+            System.out.println("FizzBuzz");
+        } else if(DivisableByThree(n)) {
+            System.out.println("Fizz");
+        } else if (DivisableByFive(n)) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(n);
+        }
+    }
+
+    private boolean DivisableByThreeAndFive(int num) {
+        return num % 3 == 0 && num % 5 == 0;
+    }
+
+    private boolean DivisableByThree(int num) {
+        return num % 3 == 0;
+    }
+
+    private boolean DivisableByFive(int num) {
+        return num % 5 == 0;
     }
 }
